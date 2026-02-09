c2f90df6406e55d7652497fdfe0bb44d8e619834
EC2 Default User <ec2-user@ip-172-31-2-229.eu-north-1.compute.internal>
2026-02-09 11:30:19 +0000

    Added the App

---
+++ index.js
@@ -0,0 +1,20 @@
+// Load http module (built into Node.js)
+const http = require('http');
+
+// Create server
+const server = http.createServer((req, res) => {
+
+  // Set response header
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  // Send response
+  res.end('Hello from my first Node app!\n');
+});
+
+// Port number
+const PORT = 3000;
+
+// Start server
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
