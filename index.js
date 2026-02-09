63d6fe795725875f445e84d6265561b0d47389d3
EC2 Default User <ec2-user@ip-172-31-2-229.eu-north-1.compute.internal>
2026-02-09 22:12:52 +0000

    Initial app

--- index.js
+++ index.js
@@ -8,14 +8,7 @@ const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
 
   // Send response
-  res.end('Mr. Money with the vibe  til the devil say my name
-Ibẹ lowa, mo ní, ibẹ ló ńgbé
-Ogedengbe, kí ló dé t olọ gbé?
-Abena, ṣébí Accra lóń gbé
-Ṣ ọmọ 2:30 for the pool party
-Full branding, no be fugazi
-White Range and black Maserati
-We dey fire go, kòní dá fún anybody');
+  res.end('TESTED AND TRUSTED');
 });
 
 // Port number
