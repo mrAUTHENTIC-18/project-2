af1e15e9cd3de5f0abdc9dbb179cf1b1e5218315
EC2 Default User <ec2-user@ip-172-31-2-229.eu-north-1.compute.internal>
2026-02-11 11:35:48 +0000

    Made Changes

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
-// Load http module (built into Node.js)
-const http = require('http');
+const http = require("http");
 
-// Create server
 const server = http.createServer((req, res) => {
-
-  // Set response header
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-
-  // Send response
-  res.end('TESTED AND TRUSTED');
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("MRPALMER");
 });
 
-// Port number
-const PORT = 3000;
-
-// Start server
-server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+server.listen(3000, '0.0.0.0', () => {
+  console.log("Server running on port 3000");
 });
